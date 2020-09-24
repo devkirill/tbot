8cd9db3be0bdbd6b5c60397f5fb6c1e51ca259b4
Deviatkin Kirill <deviatkin-kirill@ya.ru>
2020-09-25 02:51:04 +0500

    Reformat code

--- src/main/kotlin/com/project/tbot/TBotApplication.kt
+++ src/main/kotlin/com/project/tbot/TBotApplication.kt
@@ -1,6 +1,34 @@
 package com.project.tbot
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.telegram.telegrambots.ApiContextInitializer
+import org.telegram.telegrambots.meta.TelegramBotsApi
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException
+import org.telegram.telegrambots.meta.generics.LongPollingBot
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
-class TBotApplication
+class TBotApplication {
+    @Value("\${token}")
+    lateinit var token: String
+
+    private final val botApi = TelegramBotsApi()
+
+    @Autowired
+    lateinit var bots: List<LongPollingBot>
+
+    init {
+        ApiContextInitializer.init()
+    }
+
+    @PostConstruct
+    fun registerBot() {
+        try {
+            bots.forEach { botApi.registerBot(it) }
+        } catch (e: TelegramApiException) {
+            e.printStackTrace()
+        }
+    }
+}
--- src/main/kotlin/com/project/tbot/bot/MangaBot.kt
+++ src/main/kotlin/com/project/tbot/bot/MangaBot.kt
@@ -1,16 +1,21 @@
 package com.project.tbot.bot
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Message
 import org.telegram.telegrambots.meta.api.objects.Update
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException
 
+@Service
+class MangaBot : TelegramLongPollingBot() {
+    @Value("\${token}")
+    lateinit var token: String
 
-class MangaBot(val token: String, val username: String = "NAME") : TelegramLongPollingBot() {
     override fun getBotToken() = token
 
-    override fun getBotUsername() = username
+    override fun getBotUsername() = "NAME"
 
     override fun onUpdateReceived(update: Update) {
         val msg = update.message
